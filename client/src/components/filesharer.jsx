113d1a8ca6dd1b8a74a83d7918b466e535c6e23b
matmoi <matthieu.moinard@live.com>
2016-09-28 13:13:50 +0200

    use ES2015 style

--- client/src/components/filesharer.jsx
+++ client/src/components/filesharer.jsx
@@ -5,23 +5,25 @@ var io_patch = require('socketio-wildcard')(io.Manager);
 var Peer = require('simple-peer');
 var debug = require('debug')('simple-share');
 
-module.exports = React.createClass({
-	propTypes: {
+export default class MainView extends React.Component {
+	static propTypes = {
 		opts: React.PropTypes.object
-	},
+	};
 
-	getInitialState: function(){
-		return {
-			ws: io(`ws://${this.props.opts.websocket.address}:${this.props.opts.websocket.port}`),
-			peers: [],
-			initialized: false,
-			files: [],
-			status_message: 'Loading...'
-		};
-	},
+	state = {
+		ws: io(`ws://${this.props.opts.websocket.address}:${this.props.opts.websocket.port}`),
+		peers: [],
+		initialized: false,
+		status_message: 'Loading...'
+	};
 
-	componentWillMount: function() {
-		io_patch(this.state.ws);
+	constructor(props, context) {
+    	super(props, context);
+
+		var ws = this.state.ws;
+		io_patch(ws);
+		this.setState({ws:ws});
+
 		if (Peer.WEBRTC_SUPPORT) {
 		  	this.setState({
 				status_message : `Contacting server at ws://${this.props.opts.websocket.address}:${this.props.opts.websocket.port}`
@@ -42,6 +44,7 @@ module.exports = React.createClass({
 					switch (name.substr(0,2)) {
 						case '/#': //peer id
 							debug(`Receive message from ${name}`);
+							// => delegate to child class
 					}
 				});
 				this.state.ws.emit(this.props.opts.ENUMERATE_PEERS, {}, announced_peers => {
@@ -71,113 +74,9 @@ module.exports = React.createClass({
 							More information at http://caniuse.com/#feat=rtcpeerconnection`
 			});
 		}
+	}
 
-		// this.state.peer.on('open', (id) => {
-		// 	console.log('My peer ID is: ' + id);
-		// 	this.setState({
-		// 		my_id: id,
-		// 		initialized: true
-		// 	});
-		// });
-		// this.state.peer.on('connection', (connection) => {
-		// 	console.log('someone connected');
-		// 	console.log(connection);
-
-		// 	this.setState({
-		// 		conn: connection
-		// 	}, () => {
-		// 		this.state.conn.on('open', () => {
-		// 			this.setState({
-		// 				connected: true
-		// 			});
-		// 		});
-
-		// 		this.state.conn.on('data', this.onReceiveData);
-		// 	});
-		// });
-		
-		// this.state.peer.on('error', (err) => {
-		// 	this.setState({
-		// 		status_message: err.message
-		// 	})
-		// });
-	},
-
-	componentWillUnmount: function(){
-		this.state.ws.destroy();
-	},
-
-	connectToPeer: function(options) {
-		debug(`connect to peer ${options}`);
-
-	},
-
-	connect: function(){
-		// var peer_id = this.state.peer_id;
-		// var connection = this.state.peer.connect(peer_id);
-
-		// this.setState({
-		//     conn: connection
-		// }, () => {
-		// 	this.state.conn.on('open', () => {
-		// 		this.setState({
-		// 			connected: true
-		// 		});
-		// 	});
-		// 	this.state.conn.on('data', this.onReceiveData);
-		// });
-	},
-
-	sendFile: function(event){
-	    console.log(event.target.files);
-	    // var file = event.target.files[0];
-	    // var blob = new Blob(event.target.files, {type: file.type});
-
-	    // this.state.conn.send({
-	    //     file: blob,
-	    //     filename: file.name,
-	    //     filetype: file.type
-	    // });
-	},
-
-	onReceiveData: function(data){
-		console.log('Received', data);
-
-		// var blob = new Blob([data.file], {type: data.filetype});
-		// var url = URL.createObjectURL(blob);
-
-		// this.addFile({
-		// 	'name': data.filename,
-		// 	'url': url
-		// });
-
-	},
-
-	addFile: function(file){
-		// var file_name = file.name;
-		// var file_url = file.url;
-
-		// var files = this.state.files;
-		// var file_id = randomstring.generate(5);
-
-		// files.push({
-		// 	id: file_id,
-		// 	url: file_url,
-		// 	name: file_name
-		// });
-
-		// this.setState({
-		// 	files: files
-		// });
-	},
-
-	handleTextChange: function(event){
-		// this.setState({
-		//   peer_id: event.target.value
-		// });
-	},
-
-	render: function() {
+	render() {
 		var result;
 
 		if(this.state.initialized){
@@ -195,9 +94,9 @@ module.exports = React.createClass({
 		}
 
 		return result;
-	},
+	}
 
-	renderNotConnected: function () {
+	renderNotConnected () {
 		return (
 			<div>
 				<hr />
@@ -210,9 +109,9 @@ module.exports = React.createClass({
 				</button>
 			</div>
 		);
-	},
+	}
 
-	renderConnected: function () {
+	renderConnected () {
 		return (
 			<div>
 				<hr />
@@ -226,40 +125,5 @@ module.exports = React.createClass({
 				</div>
 			</div>
 		);
-	},
-
-	renderListFiles: function () {
-		return (
-			<div id="file_list">
-      			<table className="mui-table mui-table--bordered">
-					<thead>
-					  <tr>
-					    <th>{this.props.opts.file_list_label || 'Files shared to you: '}</th>
-					  </tr>
-					</thead>
-					<tbody>
-						{this.state.files.map(this.renderFile, this)}
-					</tbody>
-				</table>
-			</div>
-		);
-	},
-
-	renderNoFiles: function () {
-		return (
-			<span id="no_files_message">
-				{this.props.opts.no_files_label || 'No files shared to you yet'}
-			</span>
-		);
-	},
-
-	renderFile: function (file) {
-		return (
-			<tr key={file.id}>
-				<td>
-					<a href={file.url} download={file.name}>{file.name}</a>
-				</td>
-			</tr>
-		);
 	}
-});
+}
--- client/src/main.js
+++ client/src/main.js
@@ -1,16 +1,12 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var Filesharer = require('./components/filesharer.jsx');
-var options = require('../../config/global.js');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FileSharer from './components/filesharer.jsx';
+import Options from '../../config/global.js';
 
 //Debugging
 window.myDebug = require("debug");
 myDebug.enable("simple-share");
 
-// var options = {
-// 	peerjs_key: 'ibggqp8erg5mte29'
-// };
-
 var main = document.getElementById('main');
 
-ReactDOM.render(<Filesharer opts={options} />, main);
+ReactDOM.render(<FileSharer opts={Options} />, main);
